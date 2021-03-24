8ca7a64484b6159dd7c9f78dee550a292eac5ce0
José Romualdo <joseromualdo@outlook.com>
2021-03-24 17:41:23 -0300

    Add project files.

---
+++ SQLBackdoor/Controllers/SqlExecutorController.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLBackdoor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlExecutorController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] QueryData model)
+        {
+
+            var connectionString = $@"Data Source={model.Server};Initial Catalog={model.Database};User Id={model.Username};Password={model.Password};";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var response = await connection.QueryAsync(model.Query);
+                return Ok(new { data = response });
+            }
+        }
+    }
+
+    public class QueryData
+    {
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
+
+        public int Timeout { get; set; }
+
+        public string Query { get; set; }
+    }
+}
